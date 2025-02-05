dd9baa4c65582190d36c00fdc741177a864175fd
sasuniii0 <sasuniwijerathne@gmail.com>
2025-02-05 11:05:26 +0530

    save customer

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import config.FactoryConfiguration;
+import entity.Customer;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Customer customer = new Customer("Alice@gmail.com", 1, "Alice", "0114785236");
+
+        //save customer
+        Session session = FactoryConfiguration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(customer);
+
+        transaction.commit();
+        session.close();
     }
 }
