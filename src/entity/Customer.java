de74e466f3ca64c3d828a907d3aed0dd5595ec14
sasuniii0 <sasuniwijerathne@gmail.com>
2025-02-05 10:38:45 +0530

    customer

---
+++ src/entity/Customer.java
@@ -0,0 +1,63 @@
+package entity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Customer {
+    private int id;
+    private String name;
+    private String email;
+    private String phone;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "email='" + email + '\'' +
+                ", id=" + id +
+                ", name='" + name + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+
+    public Customer(String email, int id, String name, String phone) {
+        this.email = email;
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public Customer() {
+    }
+}
